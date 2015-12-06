a9cdcf628f2805bd2bff2786f22acb4d031c9abb
Damien Benon <damien.benon@myprysm.fr>
2015-12-06 23:13:43 +0100

    Stub ok for app generator to prepare All-In-One maven project (without modules)

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,7 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
 
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
@@ -9,22 +10,64 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the spectacular ' + chalk.red('generator-alfresco') + ' generator!'
+      'Welcome to the solid ' + chalk.red('generator-alfresco') + ' generator!'
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'name',
+      message: 'Name of the project',
+      default: _.camelCase(__dirname)
+    }, {
+      type: 'input',
+      name: 'description',
+      message: 'Description of the project'
+    }, {
+      type: 'input',
+      name: 'groupId',
+      message: 'Enter the groupId',
+      default: 'com.example'
+    }, {
+      type: 'input',
+      name: 'artifactId',
+      message: 'Enter the artifactId',
+      default: function (answers) {
+        return answers.name;
+      }
+    }, {
+      type: 'input',
+      name: 'package',
+      message: 'Enter the artifactId',
+      default: function (answers) {
+        return answers.groupId + '.' + _.camelCase(answers.name);
+      },
+      validate: function (input) {
+        return (input.match(/[a-zA-Z\.]+/g) || []).length > 0;
+      }
+    }, {
+      type: 'input',
+      name: 'version',
+      message: 'Version number',
+      default: '1.0-SNAPSHOT'
+      //}, {
+      //  type: 'checkbox',
+      //  name: 'supportedLanguages',
+      //  message: 'Supported Languages',
+      //  choices: {
+      //    'English': 'en',
+      //    'French': 'fr'
+      //  },
+      //  default: 'English'
     }];
 
     this.prompt(prompts, function (props) {
       this.props = props;
-      // To access props later use this.props.someOption;
-
+      this.appname = props.name;
       done();
     }.bind(this));
+  },
+  configuring: function () {
+    this.config.save(this.props);
   },
 
   writing: function () {
@@ -32,9 +75,92 @@ module.exports = yeoman.generators.Base.extend({
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
     );
+
+    this.fs.copy(
+      this.templatePath('.gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    // SolR 4 Config
+    this.fs.copyTpl(
+      this.templatePath('solr-config/pom.xml'),
+      this.destinationPath('solr-config/pom.xml'), {
+        groupId: this.props.groupId,
+        artifactId: this.props.artifactId,
+        version: this.props.version
+      }
+    );
+
+
+    // Share config
+    this.fs.copy(
+      this.templatePath('share/src'),
+      this.destinationPath('share/src')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('share/pom.xml'),
+      this.destinationPath('share/pom.xml'), {
+        groupId: this.props.groupId,
+        artifactId: this.props.artifactId,
+        version: this.props.version
+      }
+    );
+
+    // Alfresco config
+    this.fs.copy(
+      this.templatePath('repo/src'),
+      this.destinationPath('repo/src')
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('repo/pom.xml'),
+      this.destinationPath('repo/pom.xml'), {
+        groupId: this.props.groupId,
+        artifactId: this.props.artifactId,
+        version: this.props.version
+      }
+    );
+
+    // Runner
+    this.fs.copy(
+      this.templatePath('runner/src'),
+      this.destinationPath('runner/src')
+    );
+    this.fs.copy(
+      this.templatePath('runner/test-ng'),
+      this.destinationPath('runner/test-ng')
+    );
+    this.fs.copy(
+      this.templatePath('runner/tomcat'),
+      this.destinationPath('runner/tomcat')
+    );
+
+
+    this.fs.copyTpl(
+      this.templatePath('runner/pom.xml'),
+      this.destinationPath('runner/pom.xml'), {
+        groupId: this.props.groupId,
+        artifactId: this.props.artifactId,
+        version: this.props.version
+      }
+    );
+
+
+    // Glob project pom.xml
+    this.fs.copyTpl(
+      this.templatePath('pom.xml'),
+      this.destinationPath('pom.xml'), {
+        groupId: this.props.groupId,
+        artifactId: this.props.artifactId,
+        version: this.props.version,
+        name: this.props.name,
+        description: this.props.description
+      }
+    );
   },
 
   install: function () {
-    this.installDependencies();
+    this.spawnCommand('mvn', ['dependency:resolve']);
   }
 });
--- test/app.js
+++ test/app.js
@@ -6,14 +6,37 @@ var helpers = require('yeoman-generator').test;
 describe('generator-alfresco:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+      //.withOptions({someOption: true})
+      .withPrompts({
+        name: 'projName',
+        description: 'Description of projName',
+        groupId: 'fr.myprysm',
+        artifactId: 'projname',
+        package: 'fr.myprysm.base',
+        version: '1.0.0-SNAPSHOT'
+      })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'dummyfile.txt',
+      '.gitignore',
+      'pom.xml',
+      'solr-config/pom.xml',
+      'runner/test-ng/testng-alfresco-share.xml',
+      'runner/pom.xml',
+      'repo/src/main/properties/local',
+      'repo/pom.xml',
+      'share/src/test/resources/log4j.properties',
+      'share/pom.xml'
+    ]);
+
+    assert.fileContent([
+      ['pom.xml', '<groupId>fr.myprysm</groupId>'],
+      ['pom.xml', '<artifactId>projname</artifactId>'],
+      ['pom.xml', '<name>projName</name>'],
+      ['pom.xml', '<version>1.0.0-SNAPSHOT</version>']
     ]);
   });
 });
