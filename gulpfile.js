8635670533e45bf2811bbe9ef1c62a89d854ec54
Damien Benon <damien.benon@socialunit.fr>
2015-12-07 05:06:19 +0100

    bypassed coverall

--- gulpfile.js
+++ gulpfile.js
@@ -52,12 +52,13 @@ gulp.task('test', ['pre-test'], function (cb) {
 });
 
 gulp.task('coveralls', ['test'], function () {
-  if (!process.env.CI) {
-    return;
-  }
-
-  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-    .pipe(coveralls());
+  //if (process.env.CI) {
+  //  return;
+  //}
+  //
+  //return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+  //  .pipe(coveralls());
+  return;
 });
 
 gulp.task('prepublish', ['nsp']);
