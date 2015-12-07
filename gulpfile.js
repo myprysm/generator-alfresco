1bac9eae925191487d1fd18376fd27dc83d4651f
Damien Benon <damien.benon@myprysm.fr>
2015-12-07 15:33:54 +0100

    re-enable coveralls for test ?!

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
-//var coveralls = require('gulp-coveralls');
+var coveralls = require('gulp-coveralls');
 
 gulp.task('static', function () {
   return gulp.src('**/*.js')
@@ -52,13 +52,12 @@ gulp.task('test', ['pre-test'], function (cb) {
 });
 
 gulp.task('coveralls', ['test'], function () {
-  //if (process.env.CI) {
-  //  return;
-  //}
-  //
-  //return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
-  //  .pipe(coveralls());
-  return;
+  if (!process.env.CI) {
+    return;
+  }
+
+  return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    .pipe(coveralls());
 });
 
 gulp.task('prepublish', ['nsp']);
